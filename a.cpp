b7a32b22c95716447aead9cdc54d1c4c688dd87d
junho-baek <junho6610@yonsei.ac.kr>
2025-01-03 05:34:20 +0900

    1.2

--- a.cpp
+++ a.cpp
@@ -1,7 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-    cout << 1 << "\n";
+#define PI 3.141592
+#define loop(x,n) for(int x = 0; x <10; x++)
+int main(){
+    cout << PI << "\n";
+    int sum = 0;
+    loop(x,10){
+        sum += x;
+    }
+    cout <<sum << "\n";
     return 0;
-}       
+}
