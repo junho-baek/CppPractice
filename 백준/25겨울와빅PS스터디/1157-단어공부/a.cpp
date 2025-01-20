a5d6a73028b0aa168ab9e6ee3820b9405cf02636
junho-baek <junho6610@yonsei.ac.kr>
2025-01-20 22:17:54 +0900

    1. 20

---
+++ 백준/25겨울와빅PS스터디/1157-단어공부/a.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s;
+map<char, int> mp;
+
+bool nested = false;
+int maxInt = 0;
+char maxChar;
+
+int main(){
+    cin >> s;
+    for(char i : s){
+        mp[toupper(i)]++;
+    };
+    for(auto i :mp){
+        if(maxInt < i.second){
+            nested = false;
+            maxInt = i.second;
+            maxChar = i.first;
+        }
+        else if(maxInt == i.second){
+            nested = true;
+        }
+    } 
+
+    if(nested){
+        cout << "?" << "\n";
+    }
+    else{
+        cout << maxChar << "\n";
+    }
+    return 0;
+}
