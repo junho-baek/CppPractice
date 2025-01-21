82b645c2da7567e979d90315368a4ecedefcba6a
junho-baek <junho6610@yonsei.ac.kr>
2025-01-21 20:00:53 +0900

    2816

--- 백준/25겨울와빅PS스터디/2816-디지털티비/a.cpp
+++ 백준/25겨울와빅PS스터디/2816-디지털티비/a.cpp
@@ -17,34 +17,43 @@ int main(){
         }
     }
     
-    if(cnt1 > cnt2){
-        for(int i = 0 ; i < cnt1 ; i++){
-            r += "1";
-        }
-        for(int i = 0 ; i < cnt1 ; i++){
-            r += "4";
-        }
-        for(int i = 0 ; i < cnt2 + 1 ; i++){
-            r += "1";
-        }
-        for(int i = 0 ; i < cnt2 ; i++){
-            r += "4";
-        }
-    }
-    if(cnt1 < cnt2){
-        for(int i = 0 ; i < cnt1 ; i++){
-            r += "1";
-        }
-        for(int i = 0 ; i < cnt1 ; i++){
-            r += "4";
-        }
-        for(int i = 0 ; i < cnt2 ; i++){
-            r += "1";
-        }
-        for(int i = 0 ; i < cnt2 - 1 ; i++){
-            r += "4";
-        }
-    }
-    cout << r << "\n";
+
+    // cnt1 > cnt2 면 cnt + 1 하고 같은 문법했어야지 코드가 길어졌음..
+
+    if(cnt1 > cnt2) cnt2 += 1;
+    for(int i = 0 ; i < cnt1 ; i++) cout << "1";
+    for(int i = 0 ; i < cnt1 ; i++) cout << "4";
+    for(int i = 0 ; i < cnt2 ; i++) cout << "1";
+    for(int i = 0 ; i < cnt2 - 1 ; i++) cout << "4";
+    cout<< "\n";  
+    // if(cnt1 > cnt2){
+    //     for(int i = 0 ; i < cnt1 ; i++){
+    //         r += "1";
+    //     }
+    //     for(int i = 0 ; i < cnt1 ; i++){
+    //         r += "4";
+    //     }
+    //     for(int i = 0 ; i < cnt2 + 1 ; i++){
+    //         r += "1";
+    //     }
+    //     for(int i = 0 ; i < cnt2 ; i++){
+    //         r += "4";
+    //     }
+    // }
+    // if(cnt1 < cnt2){
+    //     for(int i = 0 ; i < cnt1 ; i++){
+    //         r += "1";
+    //     }
+    //     for(int i = 0 ; i < cnt1 ; i++){
+    //         r += "4";
+    //     }
+    //     for(int i = 0 ; i < cnt2 ; i++){
+    //         r += "1";
+    //     }
+    //     for(int i = 0 ; i < cnt2 - 1 ; i++){
+    //         r += "4";
+    //     }
+    // }
+    // cout << r << "\n";
     return 0;
 }
